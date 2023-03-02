7c00eef52bb77efe94dcb35a8ddefb70dd03bc3a
ViktoriyaKkkk <62558636+ViktoriyaKkkk@users.noreply.github.com>
2023-03-02 17:52:37 +0300

    posts list with username

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import {BrowserRouter} from "react-router-dom";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-console.log(process.env.REACT_APP_API_URL)
 root.render(
   <React.StrictMode>
       <BrowserRouter>
